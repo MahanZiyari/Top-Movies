ea232fbdd8ecb10a0c47b48375d5decb197c8850
Mahan Ziyari <maziyar.mz.021@gmail.com>
2022-07-10 19:01:27 +0430

    feat: Finish Design MainActivity
    - add BottomNavigation and NavHostFragment to MainActivity
    - add color selector for bottomNav items
    - setup bottomNav with navController
    - setup Up button with navController
    - add app logo and name to SplashFragment
    - add custom style for textViews in themes.xml

--- app/src/main/java/mahan/topmovies/ui/MainActivity.kt
+++ app/src/main/java/mahan/topmovies/ui/MainActivity.kt
@@ -2,14 +2,41 @@ package mahan.topmovies.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import mahan.topmovies.R
 import mahan.topmovies.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     // Binding Object
     private lateinit var binding: ActivityMainBinding
+
+    // NavController
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding =  ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.apply {
+            navController = findNavController(R.id.navHost)
+            bottomNav.setupWithNavController(navController)
+
+            // with this code we can determine in which fragment bottomNavigation should be visible
+            navController.addOnDestinationChangedListener{ _, destination, _ ->
+                if (destination.id == R.id.splashFragment) {
+                    bottomNav.visibility = View.GONE
+                } else {
+                    bottomNav.visibility = View.VISIBLE
+                }
+            }
+        }
+    }
+
+    // Handling Up Button with NavController
+    override fun onNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onNavigateUp()
     }
 }
