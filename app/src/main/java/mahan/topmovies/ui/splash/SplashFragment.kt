b2d760c1dd1b532ea088f8411a76319602552ce0
Mahan Ziyari <maziyar.mz.021@gmail.com>
2022-07-11 18:04:09 +0430

    feat: add datastore for user registration check
    - add 2 sec delay on splash screen and for checking user registration

--- app/src/main/java/mahan/topmovies/ui/splash/SplashFragment.kt
+++ app/src/main/java/mahan/topmovies/ui/splash/SplashFragment.kt
@@ -5,12 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.coroutineScope
+import androidx.navigation.fragment.findNavController
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
+import mahan.topmovies.R
 import mahan.topmovies.databinding.FragmentSplashBinding
+import mahan.topmovies.utils.StoreUserData
+import javax.inject.Inject
 
-
+@AndroidEntryPoint
 class SplashFragment : Fragment() {
     // Binding object
     private lateinit var binding: FragmentSplashBinding
+
+    @Inject
+    lateinit var storeUserData: StoreUserData
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,5 +34,18 @@ class SplashFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        // Apply delay for showing splash content
+        lifecycle.coroutineScope.launchWhenCreated {
+            delay(2000)
+            storeUserData.getUserToken().collect{
+                if (it.isEmpty()) {
+                    // User has not registered yet
+                    findNavController().navigate(R.id.actionSplashToRegister)
+                } else {
+                    // User has registered before
+                    findNavController().navigate(R.id.actionSplashToHome)
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/mahan/topmovies/utils/Constants.kt
@@ -0,0 +1,6 @@
+package mahan.topmovies.utils
+
+object Constants {
+    const val USER_INFO_DATASTORE = "user_info_datastore"
+    const val USER_TOKEN = "user_token"
+}
