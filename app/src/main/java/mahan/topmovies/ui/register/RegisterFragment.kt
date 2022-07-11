2e44fa22623ef31ef6f8faa13356aea8f0cd7421
Mahan Ziyari <maziyar.mz.021@gmail.com>
2022-07-11 22:03:11 +0430

    feat: finish the design of fragment_register.xml - add new style for textInputs

--- app/src/main/java/mahan/topmovies/ui/register/RegisterFragment.kt
+++ app/src/main/java/mahan/topmovies/ui/register/RegisterFragment.kt
@@ -6,15 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import mahan.topmovies.R
+import mahan.topmovies.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
+
+    // Binding Object
+    private lateinit var binding: FragmentRegisterBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
 }
